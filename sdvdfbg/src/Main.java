70e534365cf786ec1825e355453a4a331036026d
Sturvi <114976816+Sturvi@users.noreply.github.com>
2022-11-01 00:12:30 +0400

    +

---
+++ sdvdfbg/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        -5x^2+6x-5=0
+    }
+}
--- test/src/Main.java
+++ test/src/Main.java
@@ -1,7 +1,46 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+    }
 
-        int x=1/2;
-        System.out.println(x);
+    public static int maxArea(int[] height) {
+        int i=0;
+        int j=height.length-1;
+        int maxSum=0;
+        while (i<j){
+            int sum=Math.min(height[i], height[j])*(j-i);
+            maxSum=sum>maxSum?sum:maxSum;
+            if (height[i]>height[j])
+                j--;
+            else
+                i++;
+        }
+        return maxSum;
+    }
+
+    public static int trap(int[] height) {
+        int start=0;
+        int end=1;
+        int maxSum=0;
+        int sum=0;
+        while (end< height.length-1){
+            if (height[start]>height[end]){
+                end++;
+            }
+            if (height[start]<=height[end]) {
+                for (int i = start+1; i < end; i++) {
+                    sum+=height[start]-height[i];
+                }
+                start=end;
+                end++;
+                maxSum+=sum;
+                sum=0;
+            }
+            if (end== height.length-1){
+                start++;
+                end=start+1;
+            }
+        }
+        return maxSum;
     }
 }
