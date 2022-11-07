d1f52a32a6890d83a5c411480907d186c7f8fb10
Sturvi <114976816+Sturvi@users.noreply.github.com>
2022-11-07 13:47:33 +0400

    +

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int end = 10;
-        boolean test = false;
-        end=test?end:++end;
-        System.out.println(end);
-        int end2 = 10;
-        end2=test?end2:end2+1;
-        System.out.println(end2);
+        int a = 5;
+        test(++a);
+    }
+
+    private static void test(int i) {
+        i += 5 + i++;
+        System.out.println(i);
     }
 }
