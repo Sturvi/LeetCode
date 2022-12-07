e72915e8e75c7f0b88192928e853a7adbcd7fb09
Sturvi <114976816+Sturvi@users.noreply.github.com>
2022-12-07 23:18:18 +0400

    L6

---
+++ L6/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows==0) return s;
+
+        ArrayList<ArrayList<String>> arrayList = new ArrayList<ArrayList<String>>();
+        for (int i = 0; i < numRows; i++) {
+            arrayList.add(new ArrayList<String>());
+        }
+        int j = 0;
+        boolean up = true;
+        for (int i = 0; i < s.length(); i++) {
+            arrayList.get(j).add(Character.toString(s.charAt(i)));
+            if (j == numRows - 1)
+                up = false;
+            else if (j == 0) {
+                up = true;
+            }
+            j = up ? ++j : --j;
+        }
+        s = "";
+        for (int i = 0; i < numRows; i++) {
+            for (int k = 0; k < arrayList.get(i).size(); k++) {
+                s += arrayList.get(i).get(k);
+            }
+        }
+        return s;
+    }
+}
