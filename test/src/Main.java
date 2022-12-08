da84a2f7f31bb987fa65ada0aca48050790357f8
Sturvi <114976816+Sturvi@users.noreply.github.com>
2022-12-08 21:18:05 +0400

    L268

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Integer a = 5656;
-        Integer b = new Integer(5656);
-        System.out.println(a==b);
-        System.out.println(a.equals(b));
+        String str = "salamaleykum";
+        System.out.println(str.indexOf("lat"));
     }
 
 
